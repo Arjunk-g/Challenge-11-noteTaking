1483432ebcf37501bac69560daf51588d5477fee
arjun gupta <arjunkarangupta100@gmail.com>
2022-11-22 03:30:06 -0500

    Now node will display link to local host which links to the html pages

--- routes/HTMLroutes.js
+++ routes/HTMLroutes.js
@@ -2,14 +2,30 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.get('*', (req, res) =>
-res.sendFile(path.join(__dirname, 'public/index.html'))
-);
+app.get('*', (req, res) =>{
+    return res.sendFile(path.join(__dirname, '../public/index.html'))
+  });
+  
+  app.get('/notes', (req, res) =>{
+  return res.sendFile(path.join(__dirname, '../public/notes.html'))
+  });
 
-app.get('/', (req, res) =>
-res.sendFile(path.join(__dirname, './public/index.html'))
-);
+  module.exports = app;
 
-app.get('/notes', (req, res) =>
-res.sendFile(path.join(__dirname, './public/notes.html'))
-);
+
+
+
+// const { Router } = require('express');
+// const path = require('path');
+// // const express = require('express').Router();
+// const app = require('express').Router();
+
+// app.get('*', (req, res) =>{
+//     res.sendFile(path.join(__dirname, './public/index.html'))
+//   });
+  
+//   app.get('/notes', (req, res) =>{
+//   return res.sendFile(path.join(__dirname, './public/notes.html'))
+//   });
+
+//   module.exports = Router;
--- server.js
+++ server.js
@@ -1,27 +1,29 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
-const path = require('path');
-const fs = require('fs');
-const PORT = process.env.PORT || 3002;
-const api = require('./public/assets/js/index.js');
+const HTMLRoute = require('./routes/HTMLroutes');
+// const APIRoute = require('./routes/APIRoutes');
+// const router = express.Router();
+// const path = require('path');
+// const fs = require('fs');
+
+// const api = require('./public/assets/js/index.js');
 
 // Middleware for parsing JSON and urlencoded form data
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 // app.use('/api', api);
 
 // shows everything in public folder?
-app.use(express.static('public'));
+app.use(express.static('/public'));
 
 
-// app.get('*', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/index.html'))
-// );
+// app.use("/api", APIRoute);
+app.use("/", HTMLRoute);
 
-// app.get('/notes', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/notes.html'))
-// );
 
-app.listen(PORT, () =>
+app.listen(PORT, () =>{
   console.log(`Take Notes Here http://localhost:${PORT} `)
-);
+});
